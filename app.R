3f76c52c9f6ff5cfee631972f42291d8aa6d5807
bogdanoancea <bogdan.oancea@gmail.com>
2019-04-22 21:29:40 +0300

    switch back to points

--- app.R
+++ app.R
@@ -17,52 +17,52 @@ ui <- fluidPage(
   
   fluidRow( class = "plotRow",
             
-# TODO height
-  column(class = "myplot", width = 12, 
-      plotOutput(outputId = "particle", height = "600px"))
+            # TODO height
+            column(class = "myplot", width = 12, 
+                   plotOutput(outputId = "particle", height = "600px"))
   ),
   
   fluidRow( class = "controlRow",
-    style = 'height:10vh',
-    column(width = 3, 
-                  fileInput("file1", "Choose CSV File w/ persons",
-                            accept = c(
-                              "text/csv",
-                              "text/comma-separated-values,text/plain",
-                              ".csv"))
-                  ),
-    column(width = 3,
-                  fileInput("file2", "Choose CSV File w/ antenna's",
-                            accept = c(
-                              "text/csv",
-                              "text/comma-separated-values, text/plain",
-                              ".csv"))
-                  
-                  ),
-
-    column(width = 3,
-                  uiOutput("sliders")
-                  )
-
-    # column(width = 3,
-    #               downloadButton(outputId = "savegif",
-    #                              label = "Save plot to gif"))
-
-          ),
+            style = 'height:10vh',
+            column(width = 3, 
+                   fileInput("file1", "Choose CSV File w/ persons",
+                             accept = c(
+                               "text/csv",
+                               "text/comma-separated-values,text/plain",
+                               ".csv"))
+            ),
+            column(width = 3,
+                   fileInput("file2", "Choose CSV File w/ antenna's",
+                             accept = c(
+                               "text/csv",
+                               "text/comma-separated-values, text/plain",
+                               ".csv"))
+                   
+            ),
+            
+            column(width = 3,
+                   uiOutput("sliders")
+            )
+            
+            # column(width = 3,
+            #               downloadButton(outputId = "savegif",
+            #                              label = "Save plot to gif"))
+            
+  ),
   fluidRow(
     column(width = 3,
            uiOutput("sel_agents")
-  )),
+    )),
   
   tags$head(tags$style("
-                        .plotRow{height:700px;}
-                        .myplot{height:600px}
-                        .controlRow{height:100px;}
+                       .plotRow{height:700px;}
+                       .myplot{height:600px}
+                       .controlRow{height:100px;}
                        ")
-            )
-)
+  )
+  )
 
-  
+
 # Define server logic 
 server <- function(input, output) {
   
@@ -95,18 +95,18 @@ server <- function(input, output) {
     
     return(tbl)
   })
-
-####################################################################  
+  
+  ####################################################################  
   
   
-#################### create UI elements ############################  
+  #################### create UI elements ############################  
   
   output$sliders <- renderUI(
     if (!is.null(agents)) {
       
-    sliders <- sliderInput("time", "TIME:", min = min(agents()[,1]),
-                            max(agents()[,1]), value = min(agents()[,1]), step = 1,
-                           animate = animationOptions(interval = 500, loop = TRUE))
+      sliders <- sliderInput("time", "TIME:", min = min(agents()[,1]),
+                             max(agents()[,1]), value = min(agents()[,1]), step = 1,
+                             animate = animationOptions(interval = 500, loop = TRUE))
     })
   
   output$sel_agents <- renderUI(
@@ -115,33 +115,33 @@ server <- function(input, output) {
       sel_agents <- selectizeInput(inputId = "sa", label = "Select agent:", choices = c("ALL", agents()[,2]), selected = "ALL")
     })
   
-####################################################################
-
+  ####################################################################
+  
   part_anim <- function(){
     
-    df1 <- agents()[agents()[,1] <= input$time, ]
-    
+    df1 <- agents()[agents()[,1] == input$time, ]
+    #df1 <- agents()[agents()[,1] <= input$time, ]
     if(input$sa == "ALL"){
-    df1 
+      df1 
     } else {
-    df1 <- df1[df1[,2] == input$sa,]  
+      df1 <- df1[df1[,2] == input$sa,]  
     }
     
     df2 <- antenna()[]
-    p <- ggplot(data = df1, aes(x = df1[,3], y = df1[,4])) + geom_point(aes(color = df1[,2], group = df1[,2], shape = ifelse(is.na(df1[,5]), "NoSIM", "SIM"), size = ifelse(is.na(df1[,5]), "NoSIM", "SIM"))) + scale_shape_manual(name = "", values = c(NoSIM = 4, SIM = 19)) + scale_size_manual(name = "", values = c(NoSIM = 3, SIM = 8)) 
-    p <- p + geom_path(data = df1[!is.na(df1[,5]), ], aes(x = df1[!is.na(df1[,5]), 3], y = df1[!is.na(df1[,5]), 4], color = df1[!is.na(df1[,5]), 2], group = df1[!is.na(df1[,5]), 2])) 
+    p <- ggplot(data = df1, aes(x = df1[,3], y = df1[,4])) + geom_point(aes(color = df1[,2], group = df1[,2], shape = ifelse(is.na(df1[,5]), "NoSIM", "SIM"), size = ifelse(is.na(df1[,5]), "NoSIM", "SIM"))) + scale_shape_manual(name = "", values = c(NoSIM = 2, SIM = 4)) + scale_size_manual(name = "", values = c(NoSIM = 2, SIM = 4)) 
+    p <- p + geom_point(data = df1[!is.na(df1[,5]), ], aes(x = df1[!is.na(df1[,5]), 3], y = df1[!is.na(df1[,5]), 4], color = df1[!is.na(df1[,5]), 2], group = df1[!is.na(df1[,5]), 2])) 
     p <- p + xlim(0,10) + ylim(0,10)
     p <- p + geom_point(shape = 8, size = 6, data = df2, aes(x = df2[,2], y = df2[,3]), colour = "#CC0000")
     p <- p + xlab(label = "Longitude") + ylab("Latitude")
     p
-
- }
-
-
+    
+  }
+  
+  
   output$particle <- renderPlot({
     req(part_anim)
-      part_anim()
-    })
+    part_anim()
+  })
   
   #  TODO gif save
   # output$savegif <- downloadHandler(
@@ -158,4 +158,3 @@ server <- function(input, output) {
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
