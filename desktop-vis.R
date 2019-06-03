e41524075bf1b59ef5a9cfabb6091333c09326aa
bogdan.oancea@gmail.com <bogdan.oancea@gmail.com>
2019-06-03 15:55:00 +0300

    new develops

--- desktop-vis.R
+++ desktop-vis.R
@@ -30,7 +30,7 @@ p <- p + scale_x_continuous(breaks = gridpointsx, minor_breaks=NULL)
 p <- p + guides(size=FALSE)+theme_bw()
 p <- p + geom_point(data = persons, aes(x = persons[,3], y = persons[,4]) ) 
 p <- p + transition_states(persons[,1], transition_length = 1, state_length = 1) + shadow_wake(wake_length = 0.005, alpha = FALSE)
-animate(p, nframes = 880, rewind = FALSE)
+animate(p, nframes = 88, rewind = FALSE)
 
 
 #read prob
@@ -40,3 +40,32 @@ for(i in 1:nrow(prob)) {
   s<- sum(prob[i, 3:ncol(prob)])
   prob[i, 3:ncol(prob)] <-  prob[i, 3:ncol(prob)]/s
 }
+
+
+min = min(prob[2,3:402])
+min = min - 0.01*min
+limits <- c(min)
+max = max(prob[2,3:402])
+
+dif = max - min
+for(i in 1:20) {
+  x <- min + i * dif/20
+  limits <- c(limits,x)
+}
+
+pf<-cut(as.numeric(prob[2,3:402]), limits, labels = c(1:20), right = TRUE)
+m<-matrix(pf, nrow= 20, ncol=20)
+m
+
+
+#######in lucru################
+ssteep = 0.2
+smid = -92.5
+power = 10
+gamma = 3.5
+distance = 6000
+S0 = 30 + 10 * log10(power)
+Sd = 10 * gamma * log10(distance)
+S = S0 - Sd
+result = 1.0 / (1 + exp(-ssteep * (S - smid) ) )
+
